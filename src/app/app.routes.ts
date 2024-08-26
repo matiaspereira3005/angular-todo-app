a42023078312fe07abff286de75f203f6d69c357
MpereiraWare <mpereira@warehousing.cl>
2024-08-26 13:32:56 -0400

    First steps with Angular in todo project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { DefaultComponent } from './shared/layouts/default/default.component';
+import { LoginComponent } from './pages/login/login.component';
+import { TodoComponent } from './pages/todo/todo.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '', component:DefaultComponent,
+        children:[{ path:'', component:LoginComponent }],
+    },
+    {
+        path: '', component:DefaultComponent,
+        children:[{ path:'todo', component:TodoComponent }],
+    },
+];
